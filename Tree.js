2deb7ed7162c2e3cc0dd7a44cc7c77914608b35d
Xau <frozzy.xfs@gmail.com>
2023-09-10 18:54:26 -0400

    Add buildTree function, add get root method, fix use sorted array

--- Tree.js
+++ Tree.js
@@ -2,9 +2,11 @@ const Node = require('./TreeNode.js');
 
 module.exports = class Tree {
     constructor(arr) {
-        this._arr = arr || null;
-        this._root = this.buildTree(arr);
+        this._arr = this.#sortAndRemoveDuplicates(arr) || null;
+        this._root = this.buildTree(this._arr);
     }
+
+    get root() { return this._root}
 
     #sortAndRemoveDuplicates(arr) {
         let results = [];
@@ -14,7 +16,19 @@ module.exports = class Tree {
         return results.sort((a, b) => a - b);;
     }
     buildTree(data) {
-        const processed = this.#sortAndRemoveDuplicates(data);
-        const rootNode = new Node();
+        if (data.length === 0) return null;
+        if (data.length === 1) return new Node(data[0]);
+
+        let start = 0;
+        let end = data.length - 1;
+        let mid = Math.floor((start + end) / 2);
+
+        const root = new Node(data[mid]);
+
+
+        root.left = this.buildTree(data.slice(0, mid));
+        root.right = this.buildTree(data.slice(mid+1));
+
+        return root;
     }
 }

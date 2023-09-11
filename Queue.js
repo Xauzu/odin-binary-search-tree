99571ca69de91ea81ae4267a547eef1d760c67b8
Xau <frozzy.xfs@gmail.com>
2023-09-10 20:58:15 -0400

    Add basic queue

---
+++ Queue.js
@@ -0,0 +1,20 @@
+module.exports = class Queue {
+    constructor(arr) {
+        this._arr = arr;
+    }
+
+    dequeue() {
+        if (this._arr.length > 0) {
+            const item = this._arr.splice(0, 1);
+            console.log(item, this._arr);
+            return item;
+        }
+        else
+            throw new Error('Empty array');
+    }
+
+    enqueue(item) {
+        this._arr = [...this._arr, item];
+        return this._arr;
+    }
+}
